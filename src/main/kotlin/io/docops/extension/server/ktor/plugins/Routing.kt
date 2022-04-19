ca03c8d4e4c937de8f86fd8f2774e35051b48904
Steve Roach <sroachst@gmail.com>
2022-04-19 19:15:11 -0400

    optimize imports

--- src/main/kotlin/io/docops/extension/server/ktor/plugins/Routing.kt
+++ src/main/kotlin/io/docops/extension/server/ktor/plugins/Routing.kt
@@ -1,32 +1,14 @@
 package io.docops.extension.server.ktor.plugins
 
-import io.docops.asciidoc.buttons.dsl.FontWeight
-import io.docops.asciidoc.buttons.dsl.PanelButton
-import io.docops.asciidoc.buttons.dsl.Panels
-import io.docops.asciidoc.buttons.dsl.panels
-import io.docops.asciidoc.buttons.service.PanelService
-import io.docops.asciidoc.buttons.service.ScriptLoader
-import io.docops.asciidoc.buttons.theme.Grouping
-import io.docops.asciidoc.buttons.theme.GroupingOrder
 import io.docops.extension.server.web.extensions
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
-import io.ktor.server.plugins.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import java.io.BufferedReader
-import java.io.ByteArrayInputStream
-import java.io.InputStreamReader
-import java.nio.charset.Charset
-import java.util.*
-import java.util.zip.GZIPInputStream
 
 
 fun Application.configureRouting() {
-    val scriptLoader = ScriptLoader()
 
 
     routing {
