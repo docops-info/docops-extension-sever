d6b8568232c9ed48461a7e93f31043df30bb45ad
Steve Roach <sroachst@gmail.com>
2022-05-15 21:56:19 -0400

    treechart editor

--- src/main/kotlin/io/docops/extension/server/ktor/plugins/Routing.kt
+++ src/main/kotlin/io/docops/extension/server/ktor/plugins/Routing.kt
@@ -14,46 +14,13 @@ fun Application.configureRouting() {
 
     routing {
         get("/") {
-            call.respondBytes(
-                //language=html
-                """
-                    <html lang='en'>
-                    <head>
-                    <title>DocOps.io Useful tools</title>
-                    <!-- Google Fonts -->
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
-                    <!-- CSS Reset -->
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
-                    <!-- Milligram CSS -->
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
-                    </head>
-                    <body>
-                    <div id='root'>
-                    <header class="Header">
-                    <section class="container">
-                    <div class="row">
-                    <div class="column column-0">
-                    <a class="Link Logo" href="/">
-                    </a></li></ul></nav></div></div></section>
-                    </header>
-                    <main class="Main"><section class="container">
-                    <div><a href='http://docops.io'>DocOps.io</a></div>
-                    <div><a href='/editor/index.html'>Editor</a></div>
-                    <div><a href='/editor/dataset.html'>Dataset</a></div>
-                    <div><a href='/editor/chart.html'>Chart</a></div>
-                    <div><a href='/editor/treechart.html'>Tree Chart</a></div>
-                    <div><a href='/editor/stacked.html'>Stacked</a></div>
-                    <div><a href='/static/echart/demo.html'>Demo Echart</a></div>
-                    </main>
-                    
-                    </div>
-                    </body>
-                    </html>
-
-                """.trimMargin()
-                    .toByteArray(),
-                ContentType.Text.Html
-            )
+            val idx = Application::class.java.classLoader.getResourceAsStream("index.html")
+            idx?.let {
+                call.respondBytes(
+                    idx.readBytes(),
+                    ContentType.Text.Html
+                )
+            }
         }
         // Static plugin. Try to access `/static/index.html`
         static("/static") {
