8fe31da521e35a1878d677da1dc2984c96d2f2ff
Steve Roach <sroachst@gmail.com>
2023-07-14 22:19:33 -0400

    Add RoadCar class for release strategy
    A new file, RoadCar.kt, was added to implement the release strategy for "RoadCar". It also includes an SVG function to create a visual representation of the RoadCar. This is part of the effort to provide better support for different types of releases in the docops ecosystem.

--- src/main/kotlin/io/docops/docopsextensionssupport/releasestrategy/ReleaseRoadMapMaker.kt
+++ src/main/kotlin/io/docops/docopsextensionssupport/releasestrategy/ReleaseRoadMapMaker.kt
@@ -10,9 +10,9 @@ class ReleaseRoadMapMaker {
     private fun createSvg(releaseStrategy: ReleaseStrategy, isPdf: Boolean = false): String {
         val str = StringBuilder()
         var startY = -125
-        var height = 550
-        if (releaseStrategy.releases.size > 2) {
-            height += (245 * (releaseStrategy.releases.size))
+        var height = 350
+        if (releaseStrategy.releases.size > 1) {
+            height += (220 * (releaseStrategy.releases.size - 1))
         }
         releaseStrategy.releases.forEachIndexed { index, release ->
             str.append(strat(release, startY, index))
@@ -24,7 +24,7 @@ class ReleaseRoadMapMaker {
                  viewBox="0 0 ${releaseStrategy.scale * 1200} ${height * releaseStrategy.scale}">
                  <desc>https://docops.io/extension</desc>
                  ${svgDefs(isPdf)}     
-                 <g transform="scale(${releaseStrategy.scale})">           
+                 <g transform="scale(${releaseStrategy.scale})">    
                 <text x="600" text-anchor="middle" y="44" font-size="32px" font-family="Arial, Helvetica, sans-serif">${releaseStrategy.title.escapeXml()}</text>
                 $str
                 </g>
@@ -41,16 +41,17 @@ class ReleaseRoadMapMaker {
             str.append("<tspan x=\"420\" dy=\"18\">* $it</tspan>")
         }
         str.append("</text>")
-        var color = "#cccccc"
+        var color = "#dddddd"
         if (index % 2 == 0) {
             color = "#fcfcfc"
         }
         //language=svg
         return """<g transform="translate(-200,$startY)" cursor="pointer">
-            <rect x="0" y="200" height="235" width="1400" fill="$color"/>
+            <rect x="0" y="200" height="235" width="1400" fill="$color" stroke='#cccccc' class='row'/>
             <circle cx="325" cy="310" r="84.5" fill-opacity="0.15" filter="url(#filter1)"/>
             <circle class="${release.type.clazz(release.type)}" cx="323" cy="307" r="73" fill="${release.type.color(release.type)}" filter="url(#Bevel)"/>
             <circle cx="323" cy="307" r="66" fill="#ffffff"/>
+            <use href="#svg2" x="305" y="340" fill="${release.type.color(release.type)}" width="40" height="40"/>
             <text x="325" y="410" text-anchor="middle" dominant-baseline="middle" class="milestoneDate" fill="${release.type.color(release.type)}">${release.date}</text>
             <text x="325" y="315" dominant-baseline="middle" stroke-width="1px" text-anchor="middle" class="milestone"
             fill="#073763">${release.type}
@@ -61,19 +62,14 @@ class ReleaseRoadMapMaker {
     }
 
     private fun svgDefs(isPdf: Boolean): String {
-        val ani = """
-            fill: transparent;
-            stroke-width: 10px;
-            stroke-dasharray: 471;
-            stroke-dashoffset: 471;
-            animation: clock-animation 2s linear infinite;
-        """.trimIndent()
+        val ani = """ fill: transparent; stroke-width: 10px; stroke-dasharray: 471; stroke-dashoffset: 471; animation: clock-animation 2s linear infinite;""".trimIndent()
         var style = """
             <style>
                     .milestone:hover { cursor: pointer; /* calculate using: (2 * PI * R) */ stroke-width: 16; stroke-opacity: 1; fill: lightblue; }
                     .milestone { font-size: 60px; font-weight: bold; font-family: Arial, Helvetica, sans-serif; }
                     .milestoneDate { font-size: 18px; font-weight: bold; font-family: Arial, Helvetica, sans-serif; }
                     .bev:hover { $ani stroke: #6cadde; } .bev2:hover { $ani stroke: #C766A0; } .bev3:hover { $ani stroke: #136e33; }
+                    .row { filter: drop-shadow(3px 5px 2px rgb(0 0 0 / 0.4)); }
                     @keyframes clock-animation {
                         0% {
                             stroke-dashoffset: 471;
@@ -121,6 +117,7 @@ class ReleaseRoadMapMaker {
                         <feMorphology in="SourceAlpha" operator="dilate" radius="2" result="OUTLINE"/>
                         <feComposite operator="out" in="OUTLINE" in2="SourceAlpha"/>
                     </filter>
+                    ${car()}
                     $style
                 </defs>
         """.trimIndent()
