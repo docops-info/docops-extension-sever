fc401506966bc55823da740a595b05a37d3a2a88
Steve Roach <sroachst@gmail.com>
2023-08-21 07:06:28 -0400

    Improve task tracking with 'done' field in RoadMaps
    Added 'done' field to the RoadMaps data class to record completed tasks. Updated map parsing and building methods to handle this new field. This enhancement provides a holistic view of project progression by keeping track of finished tasks. Also, modified .gitignore to ignore compiler-generated files, thus keeping the codebase clean.

---
+++ src/main/kotlin/io/docops/docopsextensionssupport/web/PanelToController.kt
@@ -0,0 +1,137 @@
+package io.docops.docopsextensionssupport.web
+
+import io.docops.asciidoc.buttons.dsl.Panels
+import io.docops.asciidoc.buttons.service.ScriptLoader
+import io.docops.asciidoc.buttons.theme.ButtonType
+import io.docops.docopsextensionssupport.button.Button
+import io.docops.docopsextensionssupport.button.ButtonDisplay
+import io.docops.docopsextensionssupport.button.Buttons
+import io.docops.docopsextensionssupport.support.sourceToPanel
+import io.micrometer.core.annotation.Timed
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import kotlinx.serialization.encodeToString
+import org.springframework.stereotype.Controller
+import org.springframework.util.StreamUtils
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import java.nio.charset.Charset
+
+@Controller
+@RequestMapping("/api/panel/convert")
+class PanelToController {
+    private val scriptLoader = ScriptLoader()
+    @PutMapping("/")
+    @Timed(value = "docops.PanelToController.put", histogram = true, percentiles = [0.5, 0.95])
+    fun convert(httpServletRequest: HttpServletRequest, servletResponse: HttpServletResponse){
+        val contents = StreamUtils.copyToString(httpServletRequest.inputStream, Charset.defaultCharset())
+        val imgSrc = contentsToButtons(contents, scriptLoader, false)
+        servletResponse.contentType = "text/html"
+        servletResponse.characterEncoding = "UTF-8"
+        servletResponse.status = 200
+        val writer = servletResponse.writer
+        val contentsButtons = contentsToButtons(contents, scriptLoader, false)
+        val sb = StringBuilder()
+        contentsButtons.buttons.forEach {
+            sb.append(buttonToJson(it) +",")
+        }
+        val str = buttons(removeDelimiter(sb.toString())!!)
+        writer.print(
+            """
+              $str
+                """
+        )
+        writer.flush()
+    }
+    private fun contentsToButtons(
+        contents: String,
+        scriptLoader: ScriptLoader, isPDf: Boolean
+    ): Buttons {
+
+        try {
+            val panels: Panels = sourceToPanel(contents, scriptLoader)
+
+            val p = makeButtons(panels)
+            return Buttons(
+                buttons = p.second, buttonType = p.first, useDark = false, theme = ButtonDisplay()
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw java.lang.RuntimeException(e)
+        }
+    }
+
+    fun buttons(str: String) : String
+    {
+        return  """
+            {
+                "buttons": [$str],
+                "buttonType": "REGULAR",
+                "theme": {
+  "colors": [
+    "#45618E",
+    "#A43B3B",
+    "#FFD373",
+    "#F7E67A",
+    "#01FF90",
+    "#FF6F36",
+    "#EAA213",
+    "#FFAF10",
+    "#FF7F00",
+    "#6D4F98"
+  ],
+  "scale": 1.0,
+  "columns": 3,
+  "buttonStyle": {
+    "labelStyle": "font-family: Arial, Helvetica, sans-serif; font-size: 14px; fill: #303030; letter-spacing: normal;font-weight: bold;"
+  }
+}
+             }
+        """.trimIndent()
+    }
+    fun buttonToJson(button: Button): String {
+        //language=json
+        return """
+      {
+        "label": "${button.label}",
+        "link": "${button.link}",
+        "description": "${button.description}",
+        "type": "${button.type}",
+        "date": "${button.date}",
+        "author": [${button.author?.let { toAuthor(it) }}]
+      }
+        """.trimIndent()
+    }
+    fun removeDelimiter(s: String?): String? {
+        return if (s == null || s.isEmpty()) {
+            s
+        } else s.replaceFirst(".$".toRegex(), "")
+    }
+    fun toAuthor(authors: MutableList<String>): String? {
+        val sb = StringBuilder()
+        authors.forEach {
+            sb.append("\"$it\",")
+        }
+        return removeDelimiter(sb.toString())
+    }
+    private fun makeButtons(panels: Panels): Pair<io.docops.docopsextensionssupport.button.ButtonType, MutableList<Button>> {
+        val btns = mutableListOf<Button>()
+        val btnType: io.docops.docopsextensionssupport.button.ButtonType
+        when(panels.buttonType) {
+            ButtonType.BUTTON  -> {
+                btnType = io.docops.docopsextensionssupport.button.ButtonType.REGULAR
+            panels.panelButtons.forEach {
+                val button = Button(label = it.label, link = it.link, description = it.description, author = it.authors, date = it.date)
+                btns.add(button)
+            }
+        }
+
+            ButtonType.ROUND -> TODO()
+            ButtonType.LARGE_CARD -> TODO()
+            ButtonType.SLIM_CARD -> TODO()
+            ButtonType.RECTANGLE -> TODO()
+            ButtonType.PILL -> TODO()
+        }
+        return Pair(btnType,btns)
+    }
+}
