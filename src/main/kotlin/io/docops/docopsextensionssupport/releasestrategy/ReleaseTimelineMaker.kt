db96c8c5d54033bed107e853806ad704de02bd5d
Steve Roach <sroachst@gmail.com>
2023-10-15 08:29:59 -0400

    Update SVG elements in 'ReleaseTimelineMaker' and 'pres.svg'
    Simplified 'completedCheck' svg element and optimized 'pres.svg' for better unification and ease of maintainability. Some color, layout, and styling attributes were also updated to enhance the look and feel of the SVG image. Adjusted the size and positioning to better fit different screen sizes.

--- src/main/kotlin/io/docops/docopsextensionssupport/releasestrategy/ReleaseTimelineMaker.kt
+++ src/main/kotlin/io/docops/docopsextensionssupport/releasestrategy/ReleaseTimelineMaker.kt
@@ -198,7 +198,7 @@ open class ReleaseTimelineMaker {
                  <animateTransform attributeName="gradientTransform" type="rotate" values="360 .5 .5;0 .5 .5"
                                    dur="10s" repeatCount="indefinite" />
              </linearGradient>
-            <svg id="completedCheck" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" enable-background="new 0 0 64 64"><path d="M32,2C15.431,2,2,15.432,2,32c0,16.568,13.432,30,30,30c16.568,0,30-13.432,30-30C62,15.432,48.568,2,32,2z M25.025,50  l-0.02-0.02L24.988,50L11,35.6l7.029-7.164l6.977,7.184l21-21.619L53,21.199L25.025,50z" fill="#43a047"/></svg>
+            <polyline id="completedCheck" points="10 25 22 37 45 15" fill="none" stroke="#0f0" stroke-width="6"/>
 
              $colors
              $style
