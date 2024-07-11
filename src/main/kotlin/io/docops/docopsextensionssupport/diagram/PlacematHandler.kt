dabb65d96122da7b57963c744fdaaef69354de43
Steve Roach <sroachst@gmail.com>
2024-07-10 22:20:57 -0400

    Add support for PDF backend in PlacematHandler.
    Included an additional parameter to handleSVG and fromRequestToPlaceMat methods to ascertain if the backend is PDF. This change ensures that SVG content is correctly processed depending on the backend type.

--- src/main/kotlin/io/docops/docopsextensionssupport/diagram/PlacematHandler.kt
+++ src/main/kotlin/io/docops/docopsextensionssupport/diagram/PlacematHandler.kt
@@ -13,12 +13,13 @@ import java.net.URLDecoder
 import java.nio.charset.StandardCharsets
 
 class PlacematHandler {
-    fun handleSVG(payload: String, type: String): ResponseEntity<ByteArray> {
+    fun handleSVG(payload: String, type: String, backend: String): ResponseEntity<ByteArray> {
         val headers = HttpHeaders()
+        val isPDF = backend == "pdf"
         headers.cacheControl = CacheControl.noCache().headerValue
         headers.contentType = MediaType.parseMediaType("image/svg+xml")
         val data = uncompressString(URLDecoder.decode(payload, "UTF-8"))
-        val svg = fromRequestToPlaceMat(data, type)
+        val svg = fromRequestToPlaceMat(data, type, isPDF)
         return ResponseEntity(svg.shapeSvg.toByteArray(), headers, HttpStatus.OK)
     }
     fun handlePNG(payload: String, type: String): ResponseEntity<ByteArray> {
@@ -26,7 +27,7 @@ class PlacematHandler {
         headers.cacheControl = CacheControl.noCache().headerValue
         headers.contentType = MediaType("image", "png", StandardCharsets.UTF_8)
         val data = uncompressString(URLDecoder.decode(payload, "UTF-8"))
-        val svg = fromRequestToPlaceMat(data, type)
+        val svg = fromRequestToPlaceMat(data, type, true)
 
         val png = SvgToPng().toPngFromSvg(
             svg.shapeSvg,
@@ -34,9 +35,9 @@ class PlacematHandler {
         )
         return ResponseEntity(png, headers, HttpStatus.OK)
     }
-    fun fromRequestToPlaceMat(contents: String,  type: String = "SVG"): ShapeResponse {
+    fun fromRequestToPlaceMat(contents: String, type: String = "SVG", isPDF: Boolean): ShapeResponse {
         val pms = Json.decodeFromString<PlaceMatRequest>(contents)
-        val maker = PlaceMatMaker(placeMatRequest = pms, type)
+        val maker = PlaceMatMaker(placeMatRequest = pms, type, isPDF)
         return maker.makePlacerMat()
     }
 }
