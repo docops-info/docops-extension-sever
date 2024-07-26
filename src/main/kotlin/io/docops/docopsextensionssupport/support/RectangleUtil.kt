429ebd4304a33bd9d5bd8096d2c6b80f4a0ef7d2
Steve Roach <sroachst@gmail.com>
2024-07-25 23:27:16 -0400

    Add RectangleUtil for generating SVG path data
    This new utility function, `generateRectanglePathData`, creates path data for a rectangle with rounded corners. It accommodates individual rounding for each corner and can be useful for SVG rendering contexts. A basic example usage is included in the `main` function for verification.

---
+++ src/main/kotlin/io/docops/docopsextensionssupport/support/RectangleUtil.kt
@@ -0,0 +1,18 @@
+package io.docops.docopsextensionssupport.support
+
+fun generateRectanglePathData(width: Float, height: Float, topLetRound:Float, topRightRound:Float, bottomRightRound:Float, bottomLeftRound:Float): String {
+    return """M 0 $topLetRound 
+ A $topLetRound $topLetRound 0 0 1 $topLetRound 0
+ L ${(width - topRightRound)} 0
+ A $topRightRound $topRightRound 0 0 1 $width $topRightRound
+ L $width ${(height - bottomRightRound)}
+ A $bottomRightRound $bottomRightRound 0 0 1 ${(width - bottomRightRound)} $height
+ L $bottomLeftRound $height
+ A $bottomLeftRound $bottomLeftRound 0 0 1 0 ${(height - bottomLeftRound)}
+ Z"""
+}
+
+fun main() {
+    val path = generateRectanglePathData(200f, 300f, 12f,12f,0f,0f)
+    println(path)
+}
