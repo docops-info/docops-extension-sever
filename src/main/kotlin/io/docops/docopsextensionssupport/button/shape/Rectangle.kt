dd122656b1bc0ed1830466a6cab0985e5dbcb496
Steve Roach <sroachst@gmail.com>
2024-07-25 23:27:01 -0400

    Update Rectangle to use button color and apply style to tspan
    Modified the SVG generation for the Rectangle class to dynamically use the button's color attribute instead of a fixed color. Additionally, included the style attribute in the tspan element to apply custom styles to text spans.

--- src/main/kotlin/io/docops/docopsextensionssupport/button/shape/Rectangle.kt
+++ src/main/kotlin/io/docops/docopsextensionssupport/button/shape/Rectangle.kt
@@ -73,7 +73,7 @@ class Rectangle(buttons: Buttons) : Regular(buttons) {
             }
             var imageOrLabel = """
             <rect x="10" y="10" height="98" width="98"
-                                  class="mybox shape btn_${button.id}_cls" rx="18" ry="18" fill="#45618E"/>
+                                  class="mybox shape btn_${button.id}_cls" rx="18" ry="18" fill="${button.color}"/>
             <g class="glass" transform="translate(10,10)">
            
             <text x="49" y="68" text-anchor="middle" alignment-baseline="central"
@@ -127,7 +127,7 @@ class Rectangle(buttons: Buttons) : Regular(buttons) {
                     linkElement = link.label.escapeXml()
                 }
                 sb.append("""
-            <tspan x="115" dy="14">
+            <tspan x="115" dy="14" style="$style">
                 $linkElement
             </tspan>
                 """.trimIndent())
