bba0bd8207acca4bd3942b585b9a5b17c4598147
Steve Roach <sroachst@gmail.com>
2024-11-24 13:02:01 -0500

    Add support for donut charts
    Refactor PieSliceController to handle both pie and donut charts. Introduce DonutMaker class for generating donut charts. Make PieSliceMaker class open and protect necessary methods for reuse in DonutMaker. Update relevant tests and add SVG tooltip scripts.

--- src/test/kotlin/io/docops/docopsextensionssupport/DocopsExtensionsSupportApplicationTests.kt
+++ src/test/kotlin/io/docops/docopsextensionssupport/DocopsExtensionsSupportApplicationTests.kt
@@ -1,12 +1,6 @@
 package io.docops.docopsextensionssupport
 
-import io.docops.docopsextensionssupport.chart.DonutSlice
-import io.docops.docopsextensionssupport.chart.DonutSliceWithCommands
-import io.docops.docopsextensionssupport.chart.PieSlice
-import io.docops.docopsextensionssupport.chart.PieSlices
 import io.docops.docopsextensionssupport.chart.STUNNINGPIE
-import io.docops.docopsextensionssupport.chart.SliceDisplay
-import io.docops.docopsextensionssupport.chart.toDonutSlices
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import java.io.File
@@ -22,95 +16,6 @@ class DocopsExtensionsSupportApplicationTests {
     fun contextLoads() {
     }
 
-
-
-    class CalculusHelper {
-        fun getSlicesWithCommandsAndOffsets(
-            donutSlices: List<DonutSlice>,
-            radius: Double,
-            svgSize: Double,
-            borderSize: Double
-        ): List<DonutSliceWithCommands> {
-            var previousPercent = 0.0
-            return donutSlices.map { slice ->
-                val sliceWithCommands = DonutSliceWithCommands(
-                    id = slice.id,
-                    percent = slice.percent,
-                    amount = slice.amount,
-                    color = slice.color,
-                    label = slice.label,
-                    commands = getSliceCommands(slice, radius, svgSize, borderSize),
-                    offset = previousPercent * 3.6 * -1
-                )
-                previousPercent += slice.percent
-                sliceWithCommands
-            }
-        }
-
-        fun getSliceCommands(
-            donutSlice: DonutSlice,
-            radius: Double,
-            svgSize: Double,
-            borderSize: Double
-        ): String {
-            val degrees = percentToDegrees(donutSlice.percent)
-            val longPathFlag = if (degrees > 180) 1 else 0
-            val innerRadius = radius - borderSize
-
-            val commands = mutableListOf<String>()
-            commands.add("M ${svgSize / 2 + radius} ${svgSize / 2}")
-            commands.add(
-                "A $radius $radius 0 $longPathFlag 0 ${getCoordFromDegrees(degrees, radius, svgSize)}"
-            )
-            commands.add(
-                "L ${getCoordFromDegrees(degrees, innerRadius, svgSize)}"
-            )
-            commands.add(
-                "A $innerRadius $innerRadius 0 $longPathFlag 1 ${svgSize / 2 + innerRadius} ${svgSize / 2}"
-            )
-            return commands.joinToString(" ")
-        }
-
-        fun getCoordFromDegrees(angle: Double, radius: Double, svgSize: Double): String {
-            val x = cos(toRadians(angle))
-            val y = sin(toRadians(angle))
-            val coordX = x * radius + svgSize / 2
-            val coordY = y * -radius + svgSize / 2
-            return "$coordX $coordY"
-        }
-
-        fun percentToDegrees(percent: Double): Double {
-            return percent * 3.6
-        }
-    }
-    val donutSlices = mutableListOf<PieSlice>(
-        PieSlice("1", amount = 50.0, label = "Slice 1"),
-        PieSlice("2", amount = 25.0, label = "Slice 2"),
-        PieSlice("3", amount = 25.0, label = "Slice 3")
-
-    )
-    val slices = PieSlices(title = "Favorite Anime",mutableListOf(PieSlice(label= "Naruto", amount = 5.0),
-        PieSlice(label = "Bleach", amount = 4.0),
-        PieSlice(label = "One Piece", amount = 9.0),
-        PieSlice(label = "One Punch Man", amount = 7.0),
-        PieSlice(label = "My Hero Academia", amount = 6.0),
-        PieSlice(label = "Demon Slayer", amount = 10.0),
-    ), SliceDisplay(donut = true) )
-
-    @Test
-    fun makeDonut() {
-        val helper = CalculusHelper()
-        val commands = helper.getSlicesWithCommandsAndOffsets(slices.toDonutSlices(), 200.0, 400.0, 50.0)
-        val sb = StringBuilder()
-        sb.append("""<svg viewBox="0 0 400 400">""")
-        commands.forEach {
-            sb.append("""
-                <path d="${it.commands}" fill="${it.color}" transform="rotate(${it.offset})" class="wedge" style="transform-origin: center;"/>
-            """.trimIndent())
-        }
-        sb.append("</svg>")
-        println(sb.toString())
-    }
 
 }
 
