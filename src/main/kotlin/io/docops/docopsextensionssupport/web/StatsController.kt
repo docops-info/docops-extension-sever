3f6bd681808cf869684fc7a3d259331e5bd86f0e
Steve Roach <sroachst@gmail.com>
2025-04-02 11:01:05 -0400

    Add endpoint to generate and serve document stats as SVG
    Introduced a new `/stats/doc` endpoint to serve SVG images displaying document statistics like reading time and word count. This includes generating dynamic SVGs with customizable values via request parameters.

--- src/main/kotlin/io/docops/docopsextensionssupport/web/StatsController.kt
+++ src/main/kotlin/io/docops/docopsextensionssupport/web/StatsController.kt
@@ -1,13 +1,15 @@
 package io.docops.docopsextensionssupport.web
 
+import io.docops.docopsextensionssupport.button.shape.joinXmlLines
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.event.EventListener
-import org.springframework.http.MediaType
+import org.springframework.http.*
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import java.time.Duration
 import java.time.LocalTime
@@ -51,6 +53,45 @@ class StatsController @Autowired constructor(private val applicationEventPublish
         errorEmitters.forEach(Consumer { o: SseEmitter? -> clients.remove(o) })
     }
 
+    @GetMapping("/stats/doc", produces = ["image/svg+xml"])
+    fun getDocStats(@RequestParam("mins") mins: String,
+                    @RequestParam("count") count: String,): ResponseEntity<ByteArray> {
+        val headers = HttpHeaders()
+        headers.cacheControl = CacheControl.noCache().headerValue
+        headers.contentType = MediaType.parseMediaType("image/svg+xml")
+        return ResponseEntity(joinXmlLines("""
+           <svg xmlns="http://www.w3.org/2000/svg" width="173" height="40" viewBox="0 0 390 120">
+    <defs>
+    <linearGradient id="grad1" x2="0%" y2="100%">
+        <stop class="stop1" offset="0%" stop-color="#153e69"/>
+        <stop class="stop2" offset="50%" stop-color="#123458"/>
+        <stop class="stop3" offset="100%" stop-color="#0e2946"/>
+    </linearGradient>
+    <linearGradient id="grad2" x2="0%" y2="100%">
+        <stop class="stop1" offset="0%" stop-color="#ffffff"/>
+        <stop class="stop2" offset="50%" stop-color="#F2EFE7"/>
+        <stop class="stop3" offset="100%" stop-color="#c1bfb8"/>
+    </linearGradient>
+    </defs>
+    <rect x="0" y="0" width="100%" height="100%" fill="url(#grad1)"/>
+    <rect x="5" y="10" width="185" height="100" fill="url(#grad1)" rx="5" ry="5" stroke="#a1a1a1" stroke-width="1"/>
+    <rect x="195" y="10" width="185" height="100" fill="url(#grad1)" rx="5" ry="5" stroke="#a1a1a1" stroke-width="1"/>
+
+    <g transform="translate(5,10)">
+        <text x="92.5" y="30" text-anchor="middle" style="font-family: Helvetica,Arial,sans-serif;" fill="#fcfcfc">
+            <tspan x="92.5" style="font-weight: bold;  font-size:14px; fill: #fcfcfc;">Estimated reading time</tspan>
+            <tspan x="92.5" dy="40" style="font-weight: bold; font-size: 36px; fill: #1b98f8;">$mins Mins</tspan>
+        </text>
+    </g>
+    <g transform="translate(195,10)">
+    <text x="92.5" y="30" text-anchor="middle"  style="font-family: Helvetica,Arial,sans-serif;" fill="#fcfcfc">
+        <tspan x="92.5"  style="font-weight: bold; font-size: 14px; fill: #fcfcfc;">Word count</tspan>
+        <tspan x="92.5" dy="40" style="font-weight: bold; font-size: 36px; fill: #1b98f8;">$count</tspan>
+    </text>
+    </g>
+</svg>
+        """.trimIndent()).toByteArray(), headers, HttpStatus.OK)
+    }
     private fun formatEventToHtml(event: DocOpsExtensionEvent): String {
         //language=html
         return """
